13191e4923d375643dd494b190f72ff69bef26f0
Ndobito <jcuellarmenza@gmail.com>
2023-11-19 23:54:03 -0500

    Gifs Completados

--- Desktop/Angular/03-gifs-app/src/app/gifs/gifs.module.ts
+++ Desktop/Angular/03-gifs-app/src/app/gifs/gifs.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
+import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -10,13 +12,15 @@ import { CardListComponent } from './components/card-list/card-list.component';
   declarations: [
     HomePageComponent,
     SearchBoxComponent,
-    CardListComponent
+    CardListComponent,
+    GifsCardComponent
   ],
   exports:[
     HomePageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 export class GifsModule { }
